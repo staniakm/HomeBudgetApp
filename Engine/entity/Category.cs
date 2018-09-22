cb6dcf19c9f63464451a60c2631241df020cf381
Mariusz <Mariusz@MARIUSZ_DOMOWY>
2018-09-22 21:59:15 +0200

    move entity items to separate folder

---
+++ Engine/entity/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace Engine
+{
+    public class Category
+    {
+        public Category(int iD, string name)
+        {
+            ID = iD;
+            Nazwa = name;
+        }
+
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+
+    }
+}
